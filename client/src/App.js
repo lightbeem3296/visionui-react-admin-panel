f0618dfcb4fa03813bdee63ceadae7998e26cd12
alpha <alpha5611331@gmail.com>
2024-01-12 18:49:14 +0900

    finished tailwind-css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
 import { BrowserRouter } from "react-router-dom";
-import Admin from "./admin";
+
+import AdminPage from "./admin";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Admin />
+      <AdminPage />
     </BrowserRouter>
   );
 };
--- client/src/admin/ChargeLog.js
+++ client/src/admin/ChargeLog.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
 
-class ChargeLog extends Component {
+import DataTable from "react-data-table-component";
+
+class Creedians extends Component {
   constructor(props) {
     super(props);
     this.state = { table_data: "" };
@@ -8,7 +10,7 @@ class ChargeLog extends Component {
 
   fetchTable() {
     fetch("http://localhost:9000/admin/user_creedians_charge_log")
-      .then(res => res.text())
+      .then(res => res.json())
       .then(res => this.setState({ table_data: res }));
   }
 
@@ -17,12 +19,84 @@ class ChargeLog extends Component {
   }
 
   render() {
+    const columns = [
+      {
+        name: 'User Id',
+        selector: row => row.user_id,
+        sortable: true,
+      },
+      {
+        name: 'User No',
+        selector: row => row.user_no,
+        sortable: true,
+        width: "10rem",
+      },
+      {
+        name: 'Amount',
+        selector: row => row.amount,
+        sortable: true,
+      },
+      {
+        name: 'Character Name',
+        selector: row => row.character_name,
+        sortable: true,
+        width: "10rem",
+      },
+      {
+        name: 'Character No',
+        selector: row => row.character_no,
+        sortable: true,
+        width: "12rem",
+      },
+      {
+        name: 'Charge Type',
+        selector: row => row.charge_type,
+        sortable: true,
+        width: "10rem",
+      },
+      {
+        name: 'Map',
+        selector: row => row.map,
+        sortable: true,
+      },
+      {
+        name: 'X',
+        selector: row => row.x,
+        sortable: true,
+      },
+      {
+        name: 'Y',
+        selector: row => row.y,
+        sortable: true,
+      },
+      {
+        name: 'Item Tag',
+        selector: row => row.item_tag,
+        sortable: true,
+      },
+      {
+        name: 'Item Index',
+        selector: row => row.item_index,
+        sortable: true,
+        width: "8rem",
+      },
+      {
+        name: 'Log Date',
+        selector: row => row.log_date,
+        sortable: true,
+        width: "12rem",
+      },
+      {
+        name: 'Log Index',
+        selector: row => row.log_idx,
+        sortable: true,
+      },
+    ];
+
     return (
-        <div>
-            {this.state.table_data}
-        </div>
+      <DataTable title="Charge Log" columns={columns} data={this.state.table_data} pagination />
     );
   }
 };
 
-export default ChargeLog;
+export default Creedians;
